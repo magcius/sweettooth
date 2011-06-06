db22f7a508524409f5e03fdbcbf6a394670ebbde
Jasper St. Pierre <jstpierre@mecheye.net>
2011-06-06 11:02:52 -0400

    Use built-in auth views

--- sweettooth/auth/urls.py
+++ sweettooth/auth/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls.defaults import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    url(r'login/$', 'django.contrib.auth.views.login', dict(template_name='login.html'), name='login'),
-    url(r'logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'login/$', 'django.contrib.auth.views.login',
+        dict(template_name='login.html'), name='login'),
+    url(r'logout/$', 'django.contrib.auth.views.logout',
+        dict(template_name='logout.html'), name='logout'),
     url(r'register/$', 'auth.views.register', name='register'),
 )
