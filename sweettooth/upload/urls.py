fc8c7a62b737e4f291250c4d45bf34ae944ef6da
Jasper St. Pierre <jstpierre@mecheye.net>
2011-06-21 13:24:49 -0400

    Adjust URL for new version upload, was competing with 'edit-data'.

--- sweettooth/upload/urls.py
+++ sweettooth/upload/urls.py
@@ -5,6 +5,6 @@ slug_charset = "[a-zA-Z0-9-_]"
 
 urlpatterns = patterns('upload',
     url(r'^$', 'views.upload_file', dict(slug=None), name='upload-file'),
-    url(r'(?P<slug>%s+)/$' % (slug_charset,), 'views.upload_file', name='upload-file'),
+    url(r'new-version/(?P<slug>%s+)/$' % (slug_charset,), 'views.upload_file', name='upload-file'),
     url(r'edit-data/$', 'views.upload_edit_data', name='upload-edit-data'),
 )
