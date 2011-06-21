f2d147b4c3a0760f74d568daf8fa4da983670b8e
Jasper St. Pierre <jstpierre@mecheye.net>
2011-06-21 14:31:51 -0400

    Improve admin for Extension

--- sweettooth/extensions/admin.py
+++ sweettooth/extensions/admin.py
@@ -1,8 +1,19 @@
 
 from django.contrib import admin
-from extensions.models import Extension
+from extensions.models import Extension, ExtensionVersion
+
+class ExtensionVersionAdmin(admin.TabularInline):
+    model = ExtensionVersion
+    list_display = 'version', 'source'
 
 class ExtensionAdmin(admin.ModelAdmin):
-    list_display = 'name', 'creator'
+    list_display = 'is_published', 'name', 'num_versions', 'creator'
+    list_display_links = 'name',
+
+    def num_versions(self, ext):
+        return ext.extensionversion_set.count()
+    num_versions.short_description = "#V"
+
+    inlines = [ExtensionVersionAdmin]
 
 admin.site.register(Extension, ExtensionAdmin)
