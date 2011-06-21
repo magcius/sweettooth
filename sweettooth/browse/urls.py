d3bd52b8135f61ebee285c960155143c08c4cbd6
Jasper St. Pierre <jstpierre@mecheye.net>
2011-06-21 14:31:43 -0400

    Anchor the end of all browse URLs.
    Version detail URLs weren't getting executed: the URL would match the general
    slug URL if not anchored.

--- sweettooth/browse/urls.py
+++ sweettooth/browse/urls.py
@@ -15,22 +15,22 @@ urlpatterns = patterns('browse',
              template_object_name="extensions",
              template_name="list.html"), name='ext-index'),
 
-    url(r'tags/(?P<tag>%s+)' % (slug_charset,), tagged_object_list,
+    url(r'tags/(?P<tag>%s+)/$' % (slug_charset,), tagged_object_list,
         dict(queryset_or_model=Extension,
              template_object_name="extensions",
              template_name="list.html"), name='ext-tags'),
 
-    url(r'^manifest/(?P<uuid>.+).json',
+    url(r'^manifest/(?P<uuid>.+).json$',
         manifest, name='ext-manifest'),
 
-    url(r'^download/(?P<uuid>.+).shell-extension',
+    url(r'^download/(?P<uuid>.+).shell-extension$',
         download, name='ext-download'),
 
-    url(r'^command/(?P<uuid>.+)/(?P<cmd>.+).shell-command',
+    url(r'^command/(?P<uuid>.+)/(?P<cmd>.+).shell-command$',
         command, name='ext-command'),
 
-    url(r'^(?P<slug>%s+)' % (slug_charset,), detail,
+    url(r'^(?P<slug>%s+)/$' % (slug_charset,), detail,
         dict(ver=None), name='ext-detail'),
-    url(r'^(?P<slug>%s+)/(?P<ver>\d+)' % (slug_charset,), detail),
+    url(r'^(?P<slug>%s+)/(?P<ver>\d+)/$' % (slug_charset,), detail),
 
 )
