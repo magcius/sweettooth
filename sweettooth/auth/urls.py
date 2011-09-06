c3ce18d0746cfb549594f43b36721995c568a752
Jasper St. Pierre <jstpierre@mecheye.net>
2011-09-06 13:28:38 -0400

    Use URL namespaces for auth.

--- sweettooth/auth/urls.py
+++ sweettooth/auth/urls.py
@@ -5,8 +5,8 @@ from auth.views import AutoFocusAuthenticationForm
 urlpatterns = patterns('',
     url(r'login/$', 'django.contrib.auth.views.login',
         dict(template_name='auth/login.html',
-             authentication_form=AutoFocusAuthenticationForm), name='login'),
+             authentication_form=AutoFocusAuthenticationForm), name='auth-login'),
     url(r'logout/$', 'django.contrib.auth.views.logout',
-        dict(next_page='/'), name='logout'),
-    url(r'register/$', 'auth.views.register', name='register'),
+        dict(next_page='/'), name='auth-logout'),
+    url(r'register/$', 'auth.views.register', name='auth-register'),
 )
