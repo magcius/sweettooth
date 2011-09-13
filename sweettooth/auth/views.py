bf77cfcdc6be98d4a8b3d16dc89ded01cb935b42
Jasper St. Pierre <jstpierre@mecheye.net>
2011-09-13 14:16:10 -0400

    Add reviews and extensions to profile.

--- sweettooth/auth/views.py
+++ sweettooth/auth/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import forms, models
 from django.contrib.auth.views import login, logout
 from django.shortcuts import get_object_or_404, render
 
+from review.models import CodeReview
+from extensions.models import Extension
+
 class AutoFocusAuthenticationForm(forms.AuthenticationForm):
     def __init__(self, *a, **kw):
         super(AutoFocusAuthenticationForm, self).__init__(*a, **kw)
@@ -15,14 +18,20 @@ class InlineAuthenticationForm(forms.AuthenticationForm):
             field.widget.attrs['placeholder'] = field.label
 
 def profile(request, user):
-    user = get_object_or_404(models.User, username=user)
+    userobj = get_object_or_404(models.User, username=user)
 
     template = 'auth/profile.html'
     if request.user == user:
         template = 'auth/profile-edit.html'
 
-    display_name = user.get_full_name() or request.user.username
-    return render(request, template, dict(user=user, display_name=display_name))
+    display_name = userobj.get_full_name() or userobj.username
+    extensions = Extension.objects.filter(creator=userobj)
+    reviews = CodeReview.objects.filter(reviewer=userobj)
+
+    return render(request, template, dict(user=userobj,
+                                          display_name=display_name,
+                                          extensions=extensions,
+                                          reviews=reviews,))
 
 def register(request):
     return None
