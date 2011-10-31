d8723bce56b1dbbf325ee05206f5be8f41d74180
Jasper St. Pierre <jstpierre@mecheye.net>
2011-10-31 17:36:55 -0400

    Send an email on every error report

--- sweettooth/errorreports/views.py
+++ sweettooth/errorreports/views.py
@@ -1,7 +1,11 @@
 
+from django.conf import settings
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseForbidden
 from django.shortcuts import redirect
+from django.template.loader import render_to_string
 
 from errorreports.models import ErrorReport, error_reported
 from extensions.models import ExtensionVersion
@@ -41,3 +45,28 @@ def report_error_view(request, obj):
 @model_view(ErrorReport)
 def view_error_report_view(request, obj):
     return render(request, 'errorreports/view.html', dict(report=obj))
+
+
+def send_email_on_error_reported(request, version, report, **kwargs):
+    extension = version.extension
+
+    url = request.build_absolute_uri(reverse('errorreports-view',
+                                             kwargs=dict(pk=report.pk)))
+
+    data = dict(version=version,
+                extension=extension,
+                report=report,
+                url=url)
+
+    subject = render_to_string('errorreports/report_mail_subject.txt', data).strip()
+    subject = subject.replace('\n', '')
+    subject = subject.replace('\r', '')
+
+    message = render_to_string('errorreports/report_mail.txt', data).strip()
+
+    send_mail(subject=subject,
+              message=message,
+              from_email=settings.EMAIL_SENDER,
+              recipient_list=[extension.creator.email])
+
+error_reported.connect(send_email_on_error_reported)
