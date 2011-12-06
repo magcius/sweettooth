ab5ec434e984af890397ed589d7749a23d45e22a
Jasper St. Pierre <jstpierre@mecheye.net>
2011-12-05 22:03:02 -0500

    Fix live editing of fields.

--- sweettooth/static/js/main.js
+++ sweettooth/static/js/main.js
@@ -18,7 +18,7 @@ require(['jquery', 'messages', 'extensions',
                 }
             }
 
-            $elem.editable(url, $.merge(options,
+            $elem.editable(url, $.merge(options || {},
                                 { select: true,
                                   ajaxoptions: { error: error, dataType: 'json' },
                                   callback: function(result, settings) {
