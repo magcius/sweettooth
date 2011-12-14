870a07dae79e55b78d120d3ca0feec51c56c6f63
Jasper St. Pierre <jstpierre@mecheye.net>
2011-12-14 15:40:49 -0500

    Remove an unused import

--- sweettooth/extensions/urls.py
+++ sweettooth/extensions/urls.py
@@ -1,7 +1,6 @@
 
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import direct_to_template
-from django.views.generic.list_detail import object_list
 
 from extensions import views, models, feeds
 
