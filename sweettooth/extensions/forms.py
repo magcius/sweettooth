65412beaff2df43ad20aa6c1d49b29653beeca4d
Jasper St. Pierre <jstpierre@mecheye.net>
2011-12-27 21:43:58 -0500

    upload: Make sure extension authors agree to the terms of service

--- sweettooth/extensions/forms.py
+++ sweettooth/extensions/forms.py
@@ -7,8 +7,18 @@ class UploadForm(forms.Form):
 I verify that my extension can be distributed under the terms of the GPLv2+
 """.strip(), required=False)
 
+    tos_compliant = forms.BooleanField(label="""
+I agree that GNOME Shell Extensions can remove, modify or reassign maintainership of my extension
+""".strip(), required=False)
+
     def clean_gplv2_compliant(self):
         gplv2_compliant = self.cleaned_data['gplv2_compliant']
         if not gplv2_compliant:
             raise forms.ValidationError("You must be able to distribute your extension under the terms of the GPLv2+.")
         return gplv2_compliant
+
+    def clean_tos_compliant(self):
+        tos_compliant = self.cleaned_data['tos_compliant']
+        if not tos_compliant:
+            raise forms.ValidationError("You must agree to the GNOME Shell Extensions terms of service.")
+        return tos_compliant
