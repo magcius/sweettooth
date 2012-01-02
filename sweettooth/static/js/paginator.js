2704a6e25762d189a75d79733ce0e82d6e97d02f
Jasper St. Pierre <jstpierre@mecheye.net>
2012-01-02 15:49:48 -0500

    paginator: Fix erroneous ellipses on right-hand side in certain contexts
    If our last right-hand-side context is one before the end page,
    we'd show an erroneous pair of ellipses:
    
      Previous 1 ... 3 4 *5* 6 7 ... 8 Next
    
    This is a simple off-by-one error that's easily fixed

--- sweettooth/static/js/paginator.js
+++ sweettooth/static/js/paginator.js
@@ -119,7 +119,7 @@ define(['jquery', 'jquery.hashchange'], function($) {
                 for (var i = number+1; i < contextRight; i++)
                     makeLink(i).appendTo($elem);
 
-                if (numPages - (number+context) > 1)
+                if (numPages - (number+context) > 2)
                     $elem.append($('<span>', {'class': 'ellipses'}).text("..."));
 
                 makeLink(numPages, 'last').appendTo($elem);
