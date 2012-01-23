5087055088e25756f5e8cc3c64da541dfa41c087
Jasper St. Pierre <jstpierre@mecheye.net>
2012-01-23 15:28:32 -0500

    fsui: Start turning the FSUI into two different combo boxes

--- sweettooth/static/js/fsui.js
+++ sweettooth/static/js/fsui.js
@@ -5,14 +5,6 @@
 
 require(['jquery', 'hashparamutils', 'modal'], function($, hashparamutils, modal) {
 
-    function makeLink(name, value, text) {
-        return $('<li>', {'class': 'fsui-selection-link'}).
-            text(text).
-            click(function() {
-                hashparamutils.setHashParam(name, value);
-            });
-    }
-
     var sortCriteria = {
         'name': "Name",
         'recent': "Recent",
@@ -27,7 +19,7 @@ require(['jquery', 'hashparamutils', 'modal'], function($, hashparamutils, modal
 
             function closeUI() {
                 if ($link.hasClass('selected')) {
-                    $('.fsui').fadeOut('fast', function() {
+                    $('.fsui-dropdown').fadeOut('fast', function() {
                         $(this).detach();
                     });
                     $link.removeClass('selected');
@@ -36,40 +28,43 @@ require(['jquery', 'hashparamutils', 'modal'], function($, hashparamutils, modal
                 return false;
             }
 
-            var $link = $('<a>', {'class': 'fsui-link'}).
-                text("Filtering and Sorting").
+            var hp = hashparamutils.getHashParams();
+            if (hp.sort === undefined || !sortCriteria.hasOwnProperty(hp.sort))
+                hp.sort = 'name';
+
+            var $fsui = $('<div>', {'class': 'fsui'}).appendTo($elem);
+
+            $fsui.append('<span>Sort by</span>');
+
+            var $link = $('<a>', {'class': 'fsui-dropdown-link'}).
+                append($('<span>').text(sortCriteria[hp.sort])).
+                append($('<span>', {'class': 'fsui-dropdown-link-arrow'}).text('\u2304')).
                 click(function() {
                     $(this).addClass('selected');
-                    var pos = $elem.offset();
-                    var $fsui = $('<div>', {'class': 'fsui'}).
+                    var $dropdown = $('<div>', {'class': 'fsui-dropdown'}).
                         appendTo($elem).
                         hide().
                         fadeIn('fast');
-                    modal.activateModal($fsui, closeUI);
+                    modal.activateModal($dropdown, closeUI);
 
-                    var $sortUI = $('<div>', {'class': 'fsui-sort-ui'}).
-                        appendTo($fsui).
-                        append('<h4>Sort by</h4>');
+                    var $sortUI = $('<div>').appendTo($dropdown);
 
                     var $sortUL = $('<ul>').appendTo($sortUI);
                     var sortLinks = {};
                     $.each(sortCriteria, function(key) {
-                        sortLinks[key] = makeLink('sort', key, this).
+                        sortLinks[key] = $('<li>', {'class': 'fsui-dropdown-item'}).
+                            text(this).
                             appendTo($sortUL).
                             click(function() {
+                                hashparamutils.setHashParam('sort', key);
                                 closeUI();
                             });
                     });
 
-                    var hp = hashparamutils.getHashParams();
-                    if (hp.sort === undefined)
-                        hp.sort = 'name';
-
-                    if (sortLinks.hasOwnProperty(hp.sort))
-                        sortLinks[hp.sort].addClass('selected');
+                    sortLinks[hp.sort].addClass('selected');
 
                     return false;
-                }).appendTo($elem);
+                }).appendTo($fsui);
         });
     };
 });
