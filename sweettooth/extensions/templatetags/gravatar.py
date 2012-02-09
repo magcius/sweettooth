85ab25b07ddf0ecec519c9ceea4b34874b9f0727
Jasper St. Pierre <jstpierre@mecheye.net>
2012-02-09 14:09:57 -0500

    Host "nobody.png" locally
    This is the only asset that we serve over HTTP, not HTTPS. Because
    https://planet.gnome.org has a cert error, just copy the image into
    our local tree and serve it from there.

--- sweettooth/extensions/templatetags/gravatar.py
+++ sweettooth/extensions/templatetags/gravatar.py
@@ -6,7 +6,7 @@ register = template.Library()
 GRAVATAR_BASE = "https://secure.gravatar.com/avatar/%s?%s"
 
 @register.simple_tag
-def gravatar_url(email, size=70, default="http://planet.gnome.org/heads/nobody.png"):
+def gravatar_url(email, size=70, default="/static/images/nobody.png"):
     email_md5 = hashlib.md5(email.lower()).hexdigest()
     options = urllib.urlencode(dict(d=default, s=size))
     return GRAVATAR_BASE % (email_md5, options)
