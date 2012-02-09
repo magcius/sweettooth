9298de88a55748d081b482048b5e16e853598871
Jasper St. Pierre <jstpierre@mecheye.net>
2012-02-09 14:27:59 -0500

    gravatar: Fix fallback
    It seems gravatar doesn't like relative URLs

--- sweettooth/extensions/templatetags/gravatar.py
+++ sweettooth/extensions/templatetags/gravatar.py
@@ -6,7 +6,9 @@ register = template.Library()
 GRAVATAR_BASE = "https://secure.gravatar.com/avatar/%s?%s"
 
 @register.simple_tag
-def gravatar_url(email, size=70, default="/static/images/nobody.png"):
+def gravatar_url(request, email, size=70):
     email_md5 = hashlib.md5(email.lower()).hexdigest()
+    default = request.build_absolute_uri("/static/images/nobody.png")
     options = urllib.urlencode(dict(d=default, s=size))
+
     return GRAVATAR_BASE % (email_md5, options)
