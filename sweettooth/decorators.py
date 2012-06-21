db5e34aedb161e5210c5ab16d495ecf3aaef6b7b
Jasper St. Pierre <jstpierre@mecheye.net>
2012-06-21 17:56:16 -0400

    decorators: Clean up imports

--- sweettooth/decorators.py
+++ sweettooth/decorators.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 
 import functools
