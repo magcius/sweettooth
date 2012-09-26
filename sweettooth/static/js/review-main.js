ebcfbb7cb6bca5e6c5985e722ba6bccf4ea0744c
Jasper St. Pierre <jstpierre@mecheye.net>
2012-09-26 13:43:24 -0300

    js: Fix review JS

--- sweettooth/static/js/review-main.js
+++ sweettooth/static/js/review-main.js
@@ -1,6 +1,6 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 
-define(['review'], function($) {
+define(['jquery', 'review'], function($) {
     "use strict";
 
     $(document).ready(function() {
